b9503395a1b47d65b770a610df6daa1945e8286f
NateFrogHI <n.k.mitson@gmail.com>
2017-06-29 05:01:09 -0700

    Update Physics Engine.java
    Improved jump

--- src/Main/PhysicsEngine.java
+++ src/Main/PhysicsEngine.java
@@ -10,7 +10,7 @@ package Main;
  * @author Jeezy
  */
 public class PhysicsEngine {
-  public static final int GRAVITY = 10;
+  public static final int GRAVITY = 5;
   public static final int TERMINAL_VELOCITY = 300;
   public static final int TERMINAL_SPRINT = 10;
 
