8935dd8d3293b9201e2c483898e444113f0a9835
Jeezy <Jeezy@Jeezy-PC>
2017-06-30 20:24:37 +0900

    Merge origin/master
    Conflicts:
    	src/Main/Engine.java

--- src/Main/Projectile.java
+++ src/Main/Projectile.java
@@ -14,23 +14,6 @@ public class Projectile extends Collidable {
   private int zRot;
   private double speedX, speedY; // speed will be calculated by rise/run
   
-  public Projectile(int texId, int zrot, double x, double y, double w, double h, double sX, double sY, boolean flipY) {
-    super(texId, x, y, w, h);
-    speedX = sX;
-    speedY = sY;
-    this.flipY = flipY;
-    setColor(1.0, 0.0, 0.0);
-    zRot = zrot;
-  }
-  
-  public Projectile(int texId, int zrot, double x, double y, double sX, double sY, boolean flipY) {
-    super(texId, x, y, DrawLib.getTexture(texId).getWidth(), DrawLib.getTexture(texId).getHeight());
-    speedX = sX;
-    speedY = sY;
-    this.flipY = flipY;
-    zRot = zrot;
-  }
-  
   public Projectile(int texId, int zrot, double x, double y, boolean flipY) {
     super(texId, x, y, DrawLib.getTexture(texId).getWidth(), DrawLib.getTexture(texId).getHeight());
     Point speed = calcSpeed(zrot);
@@ -51,14 +34,11 @@ public class Projectile extends Collidable {
     GL.glRotated(zRot, 0, 0, 1); // will rotate based on clicked position
     if(this.getTextureId() >= 0) {
       //GL.glRotated(flipY ? 90 : -90, 0, 0, 1); // rotate it 90 degress onto its side
-      DrawLib.drawTexturedRectangle(this.getTextureId(), width, height);
+      DrawLib.drawTexturedRectangle(this.getTextureId(), (flipY) ? -width : width, height);
     } else {
       GL.glColor3d(color[0], color[1], color[2]);
       GL.glLineWidth((float) height);
-      if(flipY) 
-        DrawLib.drawLine(0, 0, -width, 0);
-      else
-        DrawLib.drawLine(0, 0, width, 0);
+      DrawLib.drawLine(0, 0, (flipY) ? -width : width, 0);
     }
     GL.glPopMatrix();
     if(flipY) { //update location based on speed
