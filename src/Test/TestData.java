cdd5b0d9e1c83625b5c2656fc3fbfbf64d676552
NateFrogHI <n.k.mitson@gmail.com>
2017-07-06 22:40:48 -0700

    Create TestData.java
    Provides functionality to TestDisplay

---
+++ src/Test/TestData.java
@@ -0,0 +1,42 @@
+package Test;
+
+import java.time.Instant;
+
+/**
+ * TEST CLASS
+ * Test data and timer for that data
+ * 
+ * @author Nate
+ */
+public class TestData {
+    
+    private final Instant time;
+    private String testData;
+    private boolean isViewable;
+    private int timeLimit = 10;
+    
+    public TestData(String data){
+        time = Instant.now();
+        testData = data;
+        isViewable = true;
+    }
+    
+    /**
+     * Gets testData
+     * @return String of test data
+     */
+    public String getTestData(){
+        return testData;
+    }
+    
+    /**
+     * Checks to see if data has passed its time limit
+     * @return true if data has passed its time limit
+     */
+    public boolean isExpired(){
+        if(Instant.now().getEpochSecond() - time.getEpochSecond() > timeLimit){
+            isViewable = false;
+        }
+        return !isViewable;
+    }
+}
